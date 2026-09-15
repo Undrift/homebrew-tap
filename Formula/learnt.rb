edf909d40fe88ff51f8a6613bde825dc3bc3ecd3
Harris Khan <harris.a.khan2003@gmail.com>
2026-09-15 09:37:02 -0400

    Update learnt darwin-arm64 to 0.5.7

--- Formula/learnt.rb
+++ Formula/learnt.rb
@@ -4,9 +4,9 @@ class Learnt < Formula
   version "0.5.4"
   on_macos do
     on_arm do
-      version "0.5.6"
-      url "https://github.com/Undrift/homebrew-tap/releases/download/learnt-v0.5.6/learnt-darwin-arm64.tar.gz"
-      sha256 "51c6888d170aaddf50de747d9dbf6cbfea92757d3d144ab631937b3074f855fa"
+      version "0.5.7"
+      url "https://github.com/Undrift/homebrew-tap/releases/download/learnt-v0.5.7/learnt-darwin-arm64.tar.gz"
+      sha256 "5dc0975efffe01f26cd23f219660489b4b498398a880c73eb279a656006a8f11"
     end
     on_intel do
       url "https://github.com/Undrift/homebrew-tap/releases/download/learnt-v0.5.4/learnt-darwin-amd64.tar.gz"
